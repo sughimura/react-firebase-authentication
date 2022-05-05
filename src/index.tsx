1a3a460472675644834aae59ecf0ca7160768149
sughimura <nxsughi@gmail.com>
2022-05-05 16:05:39 +0900

    chore: add 'initializeFirebase'

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeFirebase } from './_services/firebasse';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+initializeFirebase();
+
 root.render(
   <React.StrictMode>
     <App />
