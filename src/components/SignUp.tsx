591207e7acb5fb1ede150ad0e99e33c39e0e4888
sughimura <nxsughi@gmail.com>
2022-05-05 16:15:34 +0900

    feat(SignUp): add Form

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,5 +1,28 @@
+import React from 'react';
+
 const SignUp = () => {
-  return <h1>ユーザ登録</h1>;
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('登録');
+  }
+  return (
+    <div>
+      <h1>ユーザ登録</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>メールアドレス</label>
+          <input name="email" type="email" placeholder="email"/>
+        </div>
+        <div>
+          <label>パスワード</label>
+          <input name="password" type="password"/>
+        </div>
+        <div>
+          <button>登録</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default SignUp;
