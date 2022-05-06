597594ce6022bce9511d54e074ce79b9a09cc4e1
sughimura <nxsughi@gmail.com>
2022-05-06 13:55:27 +0900

    feat(App): add Layout, AuthStatus

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import SignUp from './components/SignUp';
-import { AuthProvider } from './context/AuthContext';
-import { Routes, Route } from 'react-router-dom';
+import { AuthProvider, useAuth } from './context/AuthContext';
+import { Routes, Route, useNavigate, Link, Outlet } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 
@@ -12,13 +12,49 @@ function App() {
       <h1>React Firebase Authentication Example</h1>
       <div style={{ margin: '2em' }}>
         <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
+          <Route element={<Layout />}>
+            <Route path="/home" element={<Home />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+          </Route>
         </Routes>
       </div>
     </AuthProvider>
   );
+}
+
+function Layout() {
+  return (
+    <div>
+      <AuthStatus />
+
+      <ul>
+        <li>
+          <Link to="/">Public Page</Link>
+        </li>
+        <li>
+          <Link to="/protected">Protected Page</Link>
+        </li>
+      </ul>
+
+      <Outlet />
+    </div>
+  );
+}
+
+function AuthStatus() {
+  let auth = useAuth();
+  let navigate = useNavigate();
+
+  if (!auth.user) {
+    return <p>Your are not logged in.</p>
+  }
+
+  return (
+    <p>
+      Welcome {auth.user}!{" "}
+    </p>
+  )
 }
 
 export default App;
