d50b540e4be19115f61ced65824e38365ca322a2
sughimura <nxsughi@gmail.com>
2022-05-06 10:41:19 +0900

    feat: add path "/", "/login"

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import SignUp from './components/SignUp';
 import { AuthProvider } from './context/AuthContext';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -10,7 +12,9 @@ function App() {
       <div style={{ margin: '2em' }}>
         <BrowserRouter>
           <Routes>
+            <Route path="/" element={<Home />} />
             <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </BrowserRouter>
       </div>
