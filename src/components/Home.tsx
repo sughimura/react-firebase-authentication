44f05cb82e1b57a410c58db6a7673d880bdfeb7b
sughimura <nxsughi@gmail.com>
2022-05-06 11:07:19 +0900

    feat: implement ログアウト

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+import { getAuth, signOut } from 'firebase/auth';
+
 const Home = () => {
-  return <h1>ホームページ</h1>;
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+      console.log('sign out successful')
+    }).catch((error) => {
+      console.log(error);
+    });
+    navigate("/login", { replace: true })
+  }
+  return (
+    <div>
+      <h1>ホームページ</h1>
+      <button onClick={handleLogout}>ログアウト</button>
+    </div>
+  );
 };
 
 export default Home;
